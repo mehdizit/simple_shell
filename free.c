d57aec0f23a3c42e5571d4f0a06438dded9308a0
mehdizit <zitounimehdi.it@gmail.com>
2020-11-25 20:51:01 +0100

    update

--- free.c
+++ free.c
@@ -1,28 +1,28 @@
 #include "holberton.h"
 
 /**
- * free_all - frees all malloc'd space at end of main loop
- * @tokens: pointer to tokens array
- * @path: pointer to path variable
- * @line: pointer to user input buffer
- * @fullpath: pointer to full path
- * @flag: flag marking if full_path was malloc'd
+ * free_all - frees memory space
+ * @tokens: char
+ * @path: char
+ * @line: char
+ * @fullpath: char
+ * @i:integer
  * Return: void
  */
-void free_all(char **tokens, char *path, char *line, char *fullpath, int flag)
+void free_all(char **tokens, char *path, char *line, char *fullpath, int i)
 {
 	free(path);
 	free(tokens);
 	free(line);
-	if (flag == 1)
+	if (i == 1)
 		free(fullpath);
 }
 
 /**
- * free_dp - free double pointer
- * @array: double pointer to free
- * @length: length of double pointer
- * Return: void
+ * free_dp - free array of string
+ * @array: array of string
+ * @length: length of array
+ * Return: Always 0.
  */
 void free_dp(char **array, unsigned int length)
 {
