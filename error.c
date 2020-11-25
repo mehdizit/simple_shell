621dda5b328556dca0f565c82a8bf73b2843a938
mehdizit <zitounimehdi.it@gmail.com>
2020-11-25 20:45:16 +0100

    update

--- error.c
+++ error.c
@@ -1,23 +1,27 @@
 #include "holberton.h"
-
+/**
+ * errors - prints errors
+ * @error: error number
+ * Return: Always 0
+ */
 
 void errors(int error)
 {
 	switch (error)
 	{
-	case 1: 
+	case 1:
 		write(STDERR_FILENO, ERR_F, _strlen(ERR_F));
 		perror("Error");
 		break;
 
-	case 2: 
+	case 2:
 		perror("Error");
 		break;
 
-	case 3: 
+	case 3:
 		write(STDERR_FILENO, ERR_M, _strlen(ERR_M));
 		break;
-	case 4: 
+	case 4:
 		write(STDERR_FILENO, ERR_P, _strlen(ERR_P));
 		break;
 
